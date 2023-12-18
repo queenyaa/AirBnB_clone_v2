5f67683f736a625c4ceed2e34858f2d03ef5e948
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-12-18 08:50:27 -0800

    3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,23 +1,6 @@
 -- Switch to mysql database
--- USE mysql;
--- GRANT CREATE USER ON *.* TO 'root'@'localhost';
--- GRANT GRANT OPTION ON *.* TO 'root'@'localhost';
-
--- Create the database if it doesn't exit
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-
--- Create the uder if it doesn't exit
-CREATE USER IF NOT EXISTS 'hbtn_dev'@'localhost'
-IDENTIFIED BY 'hbnb_dev_pwd';
-
-GRANT ALL PRIVILEGES
-ON hbnb_dev_db.*
-TO 'hbnb_dev'@'localhost';
-
-GRANT SELECT
-ON performance_schema.*
-TO 'hbnb_dev'@'localhost';
-
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
-
--- EXIT;
