8d4f5c43e86657d4e9c3c49facb1adec041c08fa
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-12-19 19:12:15 -0800

    7

--- models/city.py
+++ models/city.py
@@ -14,8 +14,9 @@ class City(BaseModel, Base):
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'cities'
         name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey('states.id'),
-                          nullable=False)
+        state_id = Column(String(60), nullable=False,
+                          ForeignKey('states.id'))
+
         """
         places = relationship("Place", backref="cities",
                               cascade="all, delete-orphan")
--- models/state.py
+++ models/state.py
@@ -12,12 +12,12 @@ from os import getenv
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
 
     # for DBStorage
     if getenv("HBNB_TYPE_STORAGE") == 'db':
-        name = Column(String(128), nullable=False)
         cities = relationship("City", cascade="all, delete, delete-orphan",
-                              backref="states")
+                              backref="state")
 
     # For FileStorage
     else:
