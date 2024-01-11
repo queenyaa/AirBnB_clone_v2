0946e021a3071972d5f0accdb22711e60c57634e
queenyaa <ysgyan@gmail.com>
2024-01-11 13:38:58 -0800

    2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -25,12 +25,15 @@ def do_deploy(archive_path):
 
         put(archive_path, '/tmp/')
         run('sudo mkdir -p {}{}/'.format(l_path, file_n_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, l_path, file_n_ext))
+        run('sudo tar -xzf /tmp/{} -C {}{}/'.format
+            (file_n, l_path, file_n_ext))
         run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(l_path, file_n_ext))
-        run('rm -rf {}{}/web_static'.format(l_path, file_n_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(l_path, file_n_ext))
+        run('sudo mv {0}{1}/web_static/* {0}{1}/'.format(l_path, file_n_ext))
+        run('sudo rm -rf {}{}/web_static'.format(l_path, file_n_ext))
+        run('sudo rm -rf /data/web_static/current')
+        run('sudo ln -s {}{}/ /data/web_static/current'.format
+            (l_path, file_n_ext))
+        print("New version deployed!")
         return True
 
     except Exception as e:
