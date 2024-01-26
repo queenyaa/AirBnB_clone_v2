61405b3a3603640f552c8fc19e36baa0f6ed330e
queenyaa <ysgyan@gmail.com>
2024-01-26 08:37:53 -0800

    handling dynamic route

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -8,19 +8,19 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def hello():
     """ Returns - Hello HBNB """
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ Returns HBNB """
     return 'HBNB'
 
 
-@app.route('/c<text>')
+@app.route('/c<text>', strict_slashes=False)
 def c_route(text):
     """ Replace underscores with spaces"""
     formatted_text = text.replace('_', ' ')
@@ -28,4 +28,4 @@ def c_route(text):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
