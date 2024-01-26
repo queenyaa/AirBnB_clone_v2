3d0e05754615851c7a6b5687609f4d8d960c22de
queenyaa <ysgyan@gmail.com>
2024-01-26 09:10:30 -0800

    handling dynamic route

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -20,6 +20,16 @@ def hbnb():
     return 'HBNB'
 
 
+@app.route('/c', strict_slashes=False)
+def c_404():
+    response = make_response("""
+        <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
+        <title>404 Not Found</title>
+        <h1>Not Found</h1>
+        <p>The requested URL was not found on the server.  If you entered the URL manually please check your spelling and try again.</p>""", 404)
+    return response
+
+
 @app.route('/c<text>', strict_slashes=False)
 def c_route(text):
     """ Replace underscores with spaces"""
